af6f54f7165cc7082d215c0af003ebfd945bd5e8
wade <wade@Phenom>
2013-04-24 11:24:34 +0100

    @Pointcut

--- SpringDemo28 - Pointcuts & Wildcard Expressions/src/com/osgo/aspect/LoggingAspect.java
+++ SpringDemo28 - Pointcuts & Wildcard Expressions/src/com/osgo/aspect/LoggingAspect.java
@@ -2,16 +2,33 @@ package com.osgo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect
 {
-	// @Before("execution(* get*())")								// 0 arguments
+	// @Before("execution(* get*())")									// 0 arguments
 	// @Before("execution(* get*(*))")								// 1 or more arguments
-	@Before("execution(* get*(..))")								// 0 or more arguments
-	public void LoggingAdvice()
+	// @Before("execution(* get*(..))")								// 0 or more arguments
+	@Before("allGetters()")
+	public void loggingAdvice()
 	{
 		System.out.println("Advice run.  Get method called.");
-	} // end method LoggingAdvice
+	} // end method loggingAdvice
+	
+	
+	// @Before("execution(* get*())")
+	@Before("allGetters()")
+	public void secondAdvice()
+	{
+		System.out.println("Second Advice executed.");
+	} // end method secondAdvice
+	
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters()
+	{
+	} // end dummy method allGetters
+	
 	
 } // end Class LoggingAspect
