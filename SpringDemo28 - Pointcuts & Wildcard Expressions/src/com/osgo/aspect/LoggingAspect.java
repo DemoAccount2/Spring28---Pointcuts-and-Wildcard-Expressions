1ace41d55f3cc7d46113161a74295a10c04b665a
wade <wade@Phenom>
2013-04-24 10:51:27 +0100

    update @Before annotations (with comments) in LoggingAspect.java

--- SpringDemo28 - Pointcuts & Wildcard Expressions/src/com/osgo/aspect/LoggingAspect.java
+++ SpringDemo28 - Pointcuts & Wildcard Expressions/src/com/osgo/aspect/LoggingAspect.java
@@ -6,7 +6,9 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect
 {
-	@Before("execution(public * get*())")
+	// @Before("execution(* get*())")								// 0 arguments
+	// @Before("execution(* get*(*))")								// 1 or more arguments
+	@Before("execution(* get*(..))")								// 0 or more arguments
 	public void LoggingAdvice()
 	{
 		System.out.println("Advice run.  Get method called.");
